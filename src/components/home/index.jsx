833cdeb0d0df424fc4d7be54b9f077ecbc1d148d
Gimhan <gimhanrg@gmail.com>
2024-09-08 21:36:44 +0530

    alignment issue fixes

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -4,6 +4,7 @@ import AboutSection from "./about_section";
 import PackagesSection from "./packages_section";
 import ContactSection from "./contact_section";
 import TestimonialsSection from "./testimonials_section";
+import { Box } from "@mui/material";
 
 const HomeComponent = () => {
   return (
@@ -12,18 +13,26 @@ const HomeComponent = () => {
         <HeroSection />
       </section>
       <div className="secondary-section-main">
-        <section id="about">
-          <AboutSection />
-        </section>
-        <section id="packages">
-          <PackagesSection />
-        </section>
-        <section id="testimonials">
-          <TestimonialsSection />
-        </section>
-        <section id="contact">
-          <ContactSection />
-        </section>
+        <Box mt={2} py={1} px={{ xs: 0, md: 7 }}>
+          <section id="about">
+            <AboutSection />
+          </section>
+        </Box>
+        <Box mt={1} px={{ xs: 0, md: 7 }}>
+          <section id="packages">
+            <PackagesSection />
+          </section>
+        </Box>
+        <Box mt={1} px={{ xs: 2, md: 7 }}>
+          <section id="testimonials">
+            <TestimonialsSection />
+          </section>
+        </Box>
+        <Box mt={1} mb={5} px={{ xs: 0, md: 7 }}>
+          <section id="contact">
+            <ContactSection />
+          </section>
+        </Box>
       </div>
     </div>
   );
--- src/components/main_layout/Header.jsx
+++ src/components/main_layout/Header.jsx
@@ -8,6 +8,7 @@ import {
   Box,
   Button,
   Container,
+  Divider,
   IconButton,
   Menu,
   MenuItem,
@@ -89,6 +90,23 @@ const Header = () => {
               onClose={handleCloseNavMenu}
               sx={{ display: { xs: "block", md: "none" } }}
             >
+              <Typography
+                variant="h6"
+                noWrap
+                sx={{
+                  m: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                JOHN DOE
+              </Typography>
+              <Divider />
               {sections.map((section, index) => (
                 <MenuItem
                   key={index}
@@ -105,22 +123,6 @@ const Header = () => {
             </Menu>
           </Box>
 
-          <Typography
-            variant="h5"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            JOHN DOE
-          </Typography>
           <Box
             sx={{
               flexGrow: 1,
